65d4689c97b68e667ec5bf0e68db67b7d5bf9bc9
anandodedara <anandodedara56@gmail.com>
2021-01-26 13:55:12 +0530

    commit on master

---
+++ assignment-02-node-api-postgres/index.js
@@ -0,0 +1,18 @@
+const db = require('./queries')
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/cars',db.getCars)
+
+app.get('/cars/:id',db.getCarById)
+
+app.listen(port,()=>{
+    
+})
---
+++ assignment-02-node-api-postgres/queries.js
@@ -0,0 +1,39 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user:'postgres',
+    host:'localhost',
+    database:'Vehicles',
+    password:'123456',
+    port:5432
+
+})
+
+const getCars = (request,response) =>{
+    pool.query('SELECT "Cars".id , "Cars"."Name" as "CarName", "MakeId", "Make"."name" as "MakeName", "ModelId", "Models"."Name" as "ModelName" from (("Cars" inner join "Make" on "Make".id = "Cars"."MakeId") inner join "Models" on "Cars"."ModelId" = "Models".id)',
+    (error,results)=>{
+        if(error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getCarById = (request,response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('SELECT "Cars".id , "Cars"."Name" as "CarName", "MakeId", "Make"."name" as "MakeName", "ModelId", "Models"."Name" as "ModelName" from (("Cars" inner join "Make" on "Make".id = "Cars"."MakeId") inner join "Models" on "Cars"."ModelId" = "Models".id) WHERE "Cars".id = $1',
+    [id], (error,results)=>{
+        if(error){
+            response.status(500).json("Internal Error.")
+            throw error
+        }
+        response.status(200).json(results.rows)
+
+    })
+}
+
+
+module.exports = {
+    getCars,
+    getCarById
+}
