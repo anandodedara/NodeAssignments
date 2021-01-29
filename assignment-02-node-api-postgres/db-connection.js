16aaaf255c660870e8b72fd9bac97532c7353335
anandodedara <anandodedara56@gmail.com>
2021-01-29 14:18:58 +0530

    code improved

---
+++ assignment-02-node-api-postgres/db-connection.js
@@ -0,0 +1,13 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'Vehicles',
+    password: '123456',
+    port: 5432
+
+})
+
+module.exports = {
+    pool
+}
--- assignment-02-node-api-postgres/queries.js
+++ assignment-02-node-api-postgres/queries.js
@@ -1,13 +1,5 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Vehicles',
-    password: '123456',
-    port: 5432
-
-})
-
+const data = require('./data-manipulation')
+const pool = require('./db-connection').pool
 /**
  * GET all cars
  * 
@@ -56,16 +48,16 @@ const createCar = async (request, response) => {
     const makerName = request.body.makerName
     console.log(request.body)
 
-    if (await checkIfCarExists(carName)) {
+    if (await data.checkIfCarExists(carName)) {
         response.json("This car is already exists.")
         return;
     }
 
 
-    const modelId = await insertModelIfNotExists(modelName)
-    const makerId = await insertMakerIfNotExists(makerName)
+    const modelId = await data.insertModelIfNotExists(modelName)
+    const makerId = await data.insertMakerIfNotExists(makerName)
 
-    const result = await insertCar(carName, modelId, makerId)
+    const result = await data.insertCar(carName, modelId, makerId)
 
     if (result) {
         response.status(200).json("Car inserted successfully.")
@@ -88,15 +80,15 @@ const updateCar = async (request, response) => {
     const modelName = request.body.modelName
     const makerName = request.body.makerName
 
-    if (await checkIfCarExists(carName)) {
+    if (await data.checkIfCarExists(carName)) {
         response.json("This car is already exists.")
         return;
     }
 
-    const modelId = await insertModelIfNotExists(modelName)
-    const makerId = await insertMakerIfNotExists(makerName)
+    const modelId = await data.insertModelIfNotExists(modelName)
+    const makerId = await data.insertMakerIfNotExists(makerName)
 
-    let results = await updateSingleCar(carId, carName, modelId, makerId)
+    let results = await data.updateSingleCar(carId, carName, modelId, makerId)
 
 
     if (results) {
@@ -134,165 +126,6 @@ const deleteCar = async (request, response) => {
 }
 
 
-//######################################################################################
-//Data Manipulation Functions
-
-/**
- * Function to insert new record in Car table
- * @param {string} carName 
- * @param {int} modelId 
- * @param {int} makerId 
- */
-async function insertCar(carName, modelId, makerId) {
-    let query = 'INSERT INTO public."Cars" ("Name", "MakerId", "ModelId") VALUES($1, $2, $3);'
-
-    let results = await pool.query(query, [carName, makerId, modelId])
-
-    if (results.rowCount > 0) {
-        return true
-    } else {
-        return false
-    }
-
-}
-
-
-/**
- * Insert new model to database in Models table
- * @param {string} modelName 
- */
-async function insertModel(modelName) {
-    let query = `INSERT INTO public."Models" ("Name") VALUES($1) RETURNING id;`
-
-    let results = await pool.query(query, [modelName])
-
-    if (results instanceof Error) {
-        throw results
-    }
-    else if (results.rowCount > 0) {
-        console.log(`Model inserted ID is : ${results.rows[0].id}`)
-        return results.rows[0].id
-    }
-
-}
-
-/**
- * Insert new maker to database in Makers table
- * @param {string} makerName 
- */
-async function insertMaker(makerName) {
-
-    let query = 'INSERT INTO public."Makers" ("name") VALUES($1) RETURNING id;'
-
-    let results = await pool.query(query, [makerName])
-
-    if (results instanceof Error) {
-        throw results
-    }
-    else if (results.rowCount > 0) {
-        console.log(`Maker inserted ID is : ${results.rows[0].id}`)
-        return results.rows[0].id
-    }
-
-
-}
-
-
-/**
- * check if input model exists then return it's id otherwise insert that model in database
- * @param {string} modelName 
- */
-async function insertModelIfNotExists(modelName) {
-
-
-
-    let query = `SELECT id from "Models" WHERE "Models"."Name" = '${modelName}'`;
-
-
-    let results = await pool.query(query);
-
-    if (results instanceof Error) {
-        throw results
-    }
-    else if (results.rowCount > 0) {
-        console.log(`ID is : ${results.rows[0].id}`)
-        return results.rows[0].id
-    } else {
-        return await insertModel(modelName)
-    }
-
-}
-
-
-/**
- * check if input maker exists then return it's id otherwise insert that maker in database
- * @param {string} makerName 
- */
-async function insertMakerIfNotExists(makerName) {
-
-    let query = `SELECT id FROM public."Makers" WHERE "name"=$1;`;
-
-    let results = await pool.query(query, [makerName])
-
-    if (results instanceof Error) {
-        throw results
-    }
-    else if (results.rowCount > 0) {
-        console.log(`ID is : ${results.rows[0].id}`)
-        return results.rows[0].id
-    } else {
-        return await insertMaker(makerName)
-    }
-
-
-
-}
-
-/**
- * This is use to check if car exists or not by carName
- * @param {string} carName 
- */
-async function checkIfCarExists(carName) {
-
-    let query = `SELECT "Cars"."Name" from "Cars" WHERE "Cars"."Name" = '${carName}'`;
-
-    let results = await pool.query(query)
-
-    if (results instanceof Error) {
-        throw results
-    }
-    else if (results.rowCount > 0) {
-        return true
-    } else {
-        return false
-    }
-}
-
-/**
- * This is function for updating car record in database
- * 
- * @param {*} carId 
- * @param {*} carName 
- * @param {*} modelId 
- * @param {*} makerId 
- */
-async function updateSingleCar(carId, carName, modelId, makerId) {
-    let query = `UPDATE public."Cars" SET "Name"='${carName}', "MakerId"='${makerId}', "ModelId"='${modelId}' WHERE id='${carId}';`;
-
-    let results = await pool.query(query)
-
-    console.log(results)
-    if (results instanceof Error) {
-        throw results
-    }
-    else if (results.rowCount > 0) {
-        return true
-    } else {
-        return false
-    }
-}
-
-//####################################################
 
 
 module.exports = {
